78de1038b4fa49d5af60d8b1a2fff8119ea8b753
assnr home <ycassnr@gmail.com>
2014-04-08 22:14:53 +0800

    'ArPHP1.0'

--- ArPHP/Core/ArDbException.class.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Core;
-
-class ArDbException extends ArException {
-
-}
--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-// phpinfo();
-define('APP_NAME', 'scedu');
-
-define('APP_PATH', dirname(__FILE__) . '/' . APP_NAME . '/');
-
-require_once 'ArPHP/' . 'init.php';
